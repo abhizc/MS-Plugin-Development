609f5a276d490cb84a5c1d4a6fc5240a3b31a8f6
Abhishek C <abhishek.c@ext.ema.europa.eu>
2022-11-28 08:40:22 +0100

    New Plugins Created

---
+++ Plugins/TaskCreate.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Plugins
+{
+    public class TaskCreate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Extract the tracing service for use in debugging sandboxed plug-ins.  
+            // If you are not registering the plug-in in the sandbox, then you do  
+            // not have to add any tracing service related code.  
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // Obtain the organization service reference which you will need for  
+            // web service calls.  
+            IOrganizationServiceFactory serviceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity contact = (Entity)context.InputParameters["Target"];
+
+                try
+                {
+                    // Plug-in business logic goes here. 
+
+                    Entity taskRecord = new Entity("task");
+                    taskRecord.Attributes.Add("subject", "Folloup Task");
+                    taskRecord.Attributes.Add("description", "Description Folloup Task");
+                    taskRecord.Attributes.Add("scheduledend", DateTime.Now.AddDays(2));
+
+                    taskRecord.Attributes.Add("prioritycode", new OptionSetValue(2));
+
+                    //taskrecord.Attributes.Add("regardingobjectid", new EntityReference("contact", entity.Id));
+                    taskRecord.Attributes.Add("regardingobjectid", contact.ToEntityReference());
+
+
+
+                    Guid taskGuid = service.Create(taskRecord);
+
+
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in MyPlug-in.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("MyPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
